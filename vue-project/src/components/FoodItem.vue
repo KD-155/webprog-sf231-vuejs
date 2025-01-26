7335906bb8241f34c9c78ade9e1349e56983585a
Kelly Dumbrique <ksdumbrique2@student.apc.edu.ph>
2025-01-26 19:09:35 +0800

    upd

--- vue-project/src/components/FoodItem.vue
+++ vue-project/src/components/FoodItem.vue
@@ -1,38 +1,27 @@
 <template>
-  <div v-on:click="countClicks">
-    <h2>{{ name }}</h2>  
-    <p>{{ message }}</p>
-    <p id="red">You have clicked me {{ clicks }} times.</p>
-  </div>
-</template>
-
-<script>
-export default {
-  data() {
-    return {
-      name: 'Apples',
-      message: 'I like Apples',
-      clicks: 0
+    <div class="apple">
+      <h2>{{ name }}</h2>
+      <p>{{ message }}</p>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        name: 'Apples',
+        message: 'I like Apples'
+      }
     }
-  },
-  methods: {
-    countClicks() {
-      this.clicks++;
-    }
-  }
-}
-</script>
-
-<style>
-  #app > div {
+  };
+  </script>
+  
+  <style>
+    .apple {
     border: dashed black 1px;
     display: inline-block;
-    width: 120px;
     margin: 10px;
     padding: 10px;
     background-color: lightgreen;
-  }
-  #app > div:hover {
-    cursor: pointer;
   }
 </style>
--- vue-project/src/components/FoodItem2.vue
+++ vue-project/src/components/FoodItem2.vue
@@ -1,33 +1,27 @@
 <template>
-  <div v-on:click="countClicks">
-    <h2>{{ name }}</h2>  
-    <p>{{ message }}</p>
-    <p id="red">You have clicked me {{ clicks }} times.</p>
-  </div>
-</template>
-
-<script>
-export default {
-  data() {
-    return {
-      name: 'Mangoes',
-      message: 'I like Mangoes',
-      clicks: 0
+    <div  class="mango">
+      <h2>{{ name }}</h2>
+      <p>{{ message }}</p>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        name: 'Mangoes',
+        message: 'I like Mangoes'
+      }
     }
-  }
-}
-</script>
-
-<style>
-  #app > div {
+  };
+  </script>
+  
+  <style>
+    .mango {
     border: dashed black 1px;
     display: inline-block;
-    width: 120px;
     margin: 10px;
     padding: 10px;
     background-color: lightyellow;
   }
-  #app > div:hover {
-    cursor: pointer;
-  }
-</style>
+  </style>
