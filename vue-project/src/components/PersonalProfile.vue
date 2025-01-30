d2011d4f2a0b3d4a68aeb79fa341ab0c92df0907
Kelly Dumbrique <ksdumbrique2@student.apc.edu.ph>
2025-01-30 09:00:33 +0800

    k

--- vue-project/src/components/PersonalProfile.vue
+++ vue-project/src/components/PersonalProfile.vue
@@ -1,21 +1,61 @@
 <template>
-    <div class="container">
-        <header> <h1>Personal Profile Web page </h1>  </header>
-        <main>
-          <section class="main-content">
-            <div>1 About Me</div>
-            <div>2 Education / Achievements</div>
-            <div>3 Course </div>  
-            <div>4 IT Experience</div>
-            <div>5 Hobbies & Interests</div>
-            <div>6 Goals In Life / Dream</div>  
-            <div>7 Picture Gallery</div>
-          </section>
-          <aside class="sidebar">
-            </aside>
-        </main>
-        <footer> Asia Pacific College   </footer>
-      </div>
+    <div class="grid-container">
+        <div class="item1">
+          <h2>ABOUT ME</h2>
+          <img class="header-image" src="https://scontent-hkg4-1.xx.fbcdn.net/v/t1.15752-9/462572422_891494329776180_8468297886153364528_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=9f807c&_nc_eui2=AeFXDPa9Rs9JKsZ5g-_XKR5AK9zATx17-lIr3MBPHXv6UgF7ygCOxf_oayz05jmLTarpgOWVSyQlUpTI-_8A_KW4&_nc_ohc=7F79PURwa5UQ7kNvgGF14WV&_nc_zt=23&_nc_ht=scontent-hkg4-1.xx&oh=03_Q7cD1QGfsznkgPURdGcEaL5qQrkiuasUUUCw6TMhL8TksoNktA&oe=6773FFC8" class="k1" alt="Profile Picture">
+          <div class="information">
+            <p>ksdumbrique2@student.apc.edu.ph</p>
+            <a href="https://www.linkedin.com/in/kelly-dumbrique-928a7b216/" target="_blank">LinkedIn</a>
+            <p>I am currently a 2nd-Year College student, Batch 2023-2027.</p>
+            <p>A little info about me, I got curious and joined organizations which led to me having a position. At first, I only wanted to see what goes on behind-the-scenes, but one thing led to another.</p>
+          </div>
+        </div>
+    
+        <div class="item2">
+          <h3>EDUCATION / ACHIEVEMENTS</h3>
+          <ul>
+            <li>Asia Pacific College</li>
+            <li>JISSA Vice President - Internal</li>
+            <li>APC-MSC Finance Assistant Director</li>
+            <li>Graduated Senior High School with High Honors</li>
+            <li>Brand Ambassador of Gogolook Whoscall</li>
+          </ul>
+        </div>
+    
+        <div class="item3">
+          <h3>COURSE</h3>
+          <ul>
+            <li>Bachelor of Science in Computer Science specializing in Cybersecurity and Forensics</li>
+          </ul>
+        </div>
+    
+        <div class="item4">
+          <h3>IT EXPERIENCE</h3>
+          <ul>
+            <li>Technical Support</li>
+            <li>System Administration</li>
+          </ul>
+        </div>
+    
+        <div class="item5">
+          <h3>HOBBIES & INTERESTS</h3>
+          <ul>
+            <li>Watching Movies</li>
+            <li>Watching Series</li>
+            <li>Going on Nature Trips</li>
+          </ul>
+        </div>
+    
+        <div class="item6">
+          <h3>GOALS IN LIFE / DREAM</h3>
+          <ul>
+            <li>Graduate on time</li>
+            <li>Job with a Healthy Environment</li>
+            <li>Work-Life Balance</li>
+            <li>Retire Early</li>
+            <li>Live Comfortably</li>
+          </ul>
+        </div>
     </template>
     
     <script></script>
